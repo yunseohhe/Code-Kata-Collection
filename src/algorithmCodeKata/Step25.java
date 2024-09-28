5a3c7c6046dda929e1f924ff6e41bbb5cbf55a2b
yunseohhe <dbstj4566@naver.com>
2024-09-28 10:08:58 +0900

    Step25 commit

---
+++ src/algorithmCodeKata/Step25.java
@@ -0,0 +1,22 @@
+package algorithmCodeKata;
+
+import java.util.*;
+
+class Solution25 {
+    public int[] solution(int[] arr, int divisor) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for(int a : arr) {
+            if(a % divisor == 0) {
+                list.add(a);
+            }
+        }
+        if(list.size() == 0) {
+            int[] noResult = {-1};
+            return noResult;
+        }
+        Collections.sort(list);
+        int[] result = list.stream().mapToInt(Integer::intValue).toArray();
+        return result;
+    }
+}
