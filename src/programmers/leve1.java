fa112400aad0d7320d09d33e4b460910be899295
yunseohhe <dbstj4566@naver.com>
2024-11-16 16:27:41 +0900

    level1 commit

---
+++ src/programmers/leve1.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+class Solution1 {
+    public String solution(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < str1.length(); i++){
+            sb.append(str1.substring(i, i+1));
+            sb.append(str2.substring(i, i+1));
+        }
+        return sb.toString();
+    }
+}
+/**
+ * 문제 설명
+ * 길이가 같은 두 문자열 str1과 str2가 주어집니다.
+ *
+ * 두 문자열의 각 문자가 앞에서부터 서로 번갈아가면서 한 번씩 등장하는 문자열을 만들어 return 하는 solution 함수를 완성해 주세요.
+ *
+ * 제한사항
+ * 1 ≤ str1의 길이 = str2의 길이 ≤ 10
+ * str1과 str2는 알파벳 소문자로 이루어진 문자열입니다.
+ *
+ * 입출력 예
+ * str1	str2	result
+ * "aaaaa"	"bbbbb"	"ababababab"
+ */
